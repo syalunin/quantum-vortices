82dd325b32be15739097bd2e3ea0ec1603756324
Sergey Yalunin <yalunin@rop051.ph4.physik.uni-goettingen.de>
2025-03-20 19:26:58 +0100

    Update

--- repo_update.sh
+++ repo_update.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Error: No commit message provided!"
+  exit 1
+fi
+
 echo "*.x" >> .gitignore
 echo "*.log" > .gitignore
 echo "*.env" >> .gitignore
 echo "*.DS_Store" >> .gitignore
+echo "repo_update.sh" >> .gitignore
 
 git add .
-git commit -m "Update repository"
+git commit -m "$1"
 git push origin main
